9ab0b18ef806bdf700fd2fbc0bb5149dadc5532a
nithinvukkusila <vukkusilanithin@gmail.com>
2023-03-07 17:54:12 +0530

    dashboard layout- added header icons

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -3,11 +3,20 @@ import MarketInSights from "./MarketInSights";
 import UserTargets from "./sections/userTargets";
 import Performance from "./sections/performace";
 import InvestorsDetails from "./sections/investorsDetails";
+import search from '../images/search.png'
+import bell from '../images/bell.png'
+import user from '../images/user.png'
 const UserDetails = ({ isDrawerOpen }) => {
   return (
     <div className="main-block">
       <div className="heading">
         <h1> Hi user </h1>
+        <div className="header-icons">
+         <img src={search} alt="search"></img>
+         <img src={bell} alt="bell"></img>
+         <img src={user} alt="user"></img>
+        </div>
+
       </div>
       <div className="user-details">
         <div className="details">
